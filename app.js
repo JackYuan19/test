e8d6fb754a0cf029ec3f09dbbbec091cb7876c92
xiaoyuan-2821946 <2821946800@qq.com>
2020-06-14 20:05:52 +0800

    服务端脚本

---
+++ app.js
@@ -0,0 +1,27 @@
+const koa = require('koa');
+const Router = require('koa-router');
+const static = require('koa-static');
+const render = require('koa-art-template');
+const path = require('path');
+const data = require('./data/data01');
+const app = new koa();
+const router = new Router();
+
+router.get('/',async(context) => {
+  context.render('index',{
+    imageArr: data.imageArr,
+    imageArr1: data.imageArr1,
+    imageArr2: data.imageArr2,
+    imageUrl: data.imageUrl
+  });
+});
+
+render(app,{
+    root: path.join(__dirname, 'html'),
+    extname: '.html',
+    debug: process.env.NODE_ENV !== 'production'
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+app.use(static('./static/'));
+app.listen(4000);
