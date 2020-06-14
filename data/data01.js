fcd186e6c1848f4886a6ad838efcc0db5c0ea8f1
xiaoyuan-2821946 <2821946800@qq.com>
2020-06-14 20:17:57 +0800

    假数据

---
+++ data/data01.js
@@ -0,0 +1,30 @@
+exports.imageArr = [
+  '/image/16.webp',
+  '/image/17.webp',
+  '/image/18.webp'
+];
+exports.imageArr1 = [
+  '/image/13.webp',
+  '/image/14.webp',
+  '/image/15.webp'
+];
+exports.imageArr2 = [
+  '/image/10.webp',
+  '/image/11.webp',
+  '/image/12.webp'
+];
+
+exports.imageUrl = [
+  {image: '/image/Phone.png', imageHover: '/image/Phone_hover.png',content: '话费'},
+  {image: '/image/aircraft.png', imageHover: '/image/aircraft_hover.png',content: '机票'},
+  {image: '/image/hotel.png', imageHover: '/image/hotel_hover.png',content: '酒店'},
+  {image: '/image/Game.png', imageHover: '/image/Game_hover.png',content: '游戏'},
+  {image: '/image/RefuelingCard.png', imageHover: '/image/RefuelingCard_hover.png',content: '加油卡'},
+  {image: '/image/train_tickets.png', imageHover: '/image/train_tickets_hover.png',content: '火车票'},
+  {image: '/image/Crowd-funding.png', imageHover: '/image/Crowd-funding_hover.png',content: '众筹'},
+  {image: '/image/monery.png', imageHover: '/image/monery_hover.png',content: '理财'},
+  {image: '/image/WhiteStrip.png', imageHover: '/image/WhiteStrip_hover.png',content: '白条'},
+  {image: '/image/Cinema_ticket.png', imageHover: '/image/Cinema_ticket_hover.png',content: '电影票'},
+  {image: '/image/EnterprisePurchase.png', imageHover: '/image/EnterprisePurchase_hover.png',content: '企业购'},
+  {image: '/image/GiftCard.png', imageHover: '/image/GiftCard_hover.png',content: '礼品卡'},
+]
