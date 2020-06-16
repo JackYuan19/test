5a9a89163efd4083f1c3095f4accbc35e64b65f2
xiaoyuan-2821946 <2821946800@qq.com>
2020-06-16 15:11:09 +0800

    假数据

--- data/data01.js
+++ data/data01.js
@@ -27,4 +27,127 @@ exports.imageUrl = [
   {image: '/image/Cinema_ticket.png', imageHover: '/image/Cinema_ticket_hover.png',content: '电影票'},
   {image: '/image/EnterprisePurchase.png', imageHover: '/image/EnterprisePurchase_hover.png',content: '企业购'},
   {image: '/image/GiftCard.png', imageHover: '/image/GiftCard_hover.png',content: '礼品卡'},
+];
+
+exports.imageBottom = [
+  {
+    imageUrl: '/image/7c3769ee97de365d.jpg.webp',
+    currentPrice: 29.90,
+    previousPrice: 58.00,
+    content: '骆驼(CAMEL) 运动T恤男女宽松纯棉白色短袖简约透气运动休闲情侣半袖衫 C0S2XN662 男款极地白 L'
+  },
+  {
+    imageUrl: '/image/231f525c0170556f.jpg.webp',
+    currentPrice: 48.90,
+    previousPrice: 119.00,
+    content: '【限量1000套】儿童套装短袖男童套装女童纯棉T恤短裤两件套中婴小童套装夏季 闪电猫 75码(建议身高110-120cm)'
+  },
+  {
+    imageUrl: '/image/fed616e0985c1761.jpg.webp',
+    currentPrice: 699.00,
+    previousPrice: 1390.00,
+    content: 'CNC男装男士夏季时尚修身短袖POLO衫T恤休闲个性金属烫金蜘蛛图案短袖 黑色 50'
+  },
+  {
+    imageUrl: '/image/e590fd47fd3ec513.jpg.webp',
+    currentPrice: 98.00,
+    previousPrice: 145.00,
+    content: '脉鲜（MAXSUN）户外便携卡式炉 野炊瓦斯炉 自驾烧烤炉具 火锅燃气罐炉MS-3000'
+  },
+  {
+    imageUrl: '/image/67182e730bde943b.jpg.webp',
+    currentPrice: 319.00,
+    previousPrice: 498.00,
+    content: '耐克NIKE 男子 简约 休闲鞋 STRADA 运动鞋 CQ7626-002黑色42码'
+  },
+  {
+    imageUrl: '/image/0d2fde13dd37a43d.jpg.webp',
+    currentPrice: 6999.00,
+    previousPrice: 7999.00,
+    content: '佳能（Canon）EOS 90D 单反相机 单反机身（3250万有效像素 双核CMOS 双核CMOS 45点十字对焦 WIFI）'
+  },
+  {
+    imageUrl: '/image/f3204da9646c3911.jpg.webp',
+    currentPrice: 99.00,
+    previousPrice: 288.00,
+    content: '【99元超值7件装！限时抢购100套！冰丝T恤】夏季新款短袖男士圆领冰丝印花半袖T恤 【套餐1】22白+24白+26黑+28蓝+送一件 XL'
+  },
+  {
+    imageUrl: '/image/3e0f2608a79790c8.png.webp',
+    currentPrice: 330.00,
+    previousPrice: 490.00,
+    content: 'CK UNDERWEAR 2020春夏款 内裤男士内裤时尚Logo透气平角内裤3条装 U2664D 998-黑色/灰色/白色 M'
+  },
+  {
+    imageUrl: '/image/d4a4211881ec7c3c.jpg.webp',
+    currentPrice: 139.00,
+    previousPrice: 359.00,
+    content: '苏泊尔（SUPOR）炒锅不粘锅煎锅平底锅炒菜锅煎炒锅锅具D系列电磁炉明火燃气通用 28厘米 EJ28QAP-01'
+  },
+  {
+    imageUrl: '/image/af1ce3b6ff355853.jpg.webp',
+    currentPrice: 699.00,
+    previousPrice: 1399.00,
+    content: '盛世珠宝 黄金吊坠女 开心玉面金佛 3d硬金本命佛足金佛挂坠金镶玉吊坠佛公项链和田玉佛坠 欢喜萌佛'
+  },
+  {
+    imageUrl: '/image/69148d553e6bee13.jpg.webp',
+    currentPrice: 79.00,
+    previousPrice: 266.66,
+    content: '【情侣款 直降1800】【海外名品 已质检】法国轻奢巴黎走秀百搭时尚经典款手工定制帆布鞋 黑色-男款 40'
+  },
+  {
+    imageUrl: '/image/fecc67c60d49584d.jpg.webp',
+    currentPrice: 299.00,
+    previousPrice: 599.00,
+    content: '雷士（NVC）风暖浴霸高性价比基础款双电机  大功率速热取暖器 卫生间浴霸灯 浴室暖风机 适用于集成吊顶'
+  },
+  {
+    imageUrl: '/image/e36f776d795e58f4.jpg.webp',
+    currentPrice: 1445.00,
+    previousPrice: 2199.00,
+    content: '华为nova6 se 手机 绮境森林 全网通（8+128GB）'
+  },
+  {
+    imageUrl: '/image/c42483a11319deff.jpg.webp',
+    currentPrice: 598.00,
+    previousPrice: 899.00,
+    content: '康宝（Canbo）消毒柜家用 小型 立式 高温 二星级 消毒碗柜 厨房碗筷餐具家用消毒碗柜 家电 XDZ100-N1【5层架100L】'
+  },
+  {
+    imageUrl: '/image/cc9f618e799e144f.jpg.webp',
+    currentPrice: 16.80,
+    previousPrice: 49.90,
+    content: '【延吉馆】 裕源祥 延边朝鲜族特色大冷面  荞麦冷面 内含汤料 256g 三袋装'
+  },
+  {
+    imageUrl: '/image/245424b7ab630df8.png.webp',
+    currentPrice: 3299.00,
+    previousPrice: 3699.00,
+    content: '惠普(HP)小欧S01商务办公台式电脑主机（九代i5-9400 8G 1TB+256 UMA Win10 五年上门）'
+  },
+  {
+    imageUrl: '/image/74ea5495c3fcd527.jpg.webp',
+    currentPrice: 457.90,
+    previousPrice: 899.00,
+    content: 'Adidas阿迪达斯女鞋男鞋Superstar金标贝壳头三叶草C77124运动鞋小白鞋板鞋 C77124/C77154 37'
+  },
+  {
+    imageUrl: '/image/968ca19862733a3a.jpg.webp',
+    currentPrice: 3149.00,
+    previousPrice: 3299.00,
+    content: '英特尔（Intel）艾莱峡谷 NUC迷你电脑 i5-8265U+独显+8G内存 支持win10 需自配电源线（NUC8I5INHX）'
+  },
+  {
+    imageUrl: '/image/c39a98d7205afea3.jpg.webp',
+    currentPrice: 79.00,
+    previousPrice: 159.00,
+    content: 'Feiyue/新款飞跃小白鞋运动鞋帆布鞋板鞋潮流男女款 8108白金白底 42'
+  },
+  {
+    imageUrl: '/image/4e2533e3736620ef.jpg.webp',
+    currentPrice: 1298.00,
+    previousPrice: 2899.00,
+    content: '15.6英寸 超簿本 轻薄本 笔记本电脑轻薄 超级本 学生网上课堂电脑 远程办公电脑 笔记本电脑 银白色-超窄边框Intel-J3455 8G内存SSD256G硬盘'
+  }
 ]
